25700a9247d3fe81b9247892fd3f28067ff60573
M-kalharbi <formohammed332@gmail.com>
2024-04-18 00:24:44 +0300

    Task02

---
+++ src/test/java/Homework/assigment2.java
@@ -0,0 +1,50 @@
+package Homework;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+import static org.hamcrest.Matchers.equalTo;
+
+public class assigment2 {
+     /*
+       Given
+           https://reqres.in/api/users/2
+       When
+           User send GET Request to the URL
+       Then
+           HTTP Status Code should be 200
+       And
+           Response format should be "application/json"
+       And
+           "email" is "janet.weaver@reqres.in",
+       And
+           "first_name" is "Janet"
+       And
+           "last_name" is "Weaver"
+       And
+           "text" is "To keep ReqRes free, contributions towards server costs are appreciated!"
+    */
+  @Test
+  public void test() {
+    Response response = RestAssured.get("https://reqres.in/api/users/2");
+        response.prettyPrint();
+
+
+
+
+         response
+                 .then()
+                 .statusCode(200)
+                 .contentType("application/json")
+                 .body("data.email", equalTo("janet.weaver@reqres.in"),
+                 .body("data.first_name", equalTo("Janet"),
+                 .body("data.last_name",equalTo("Weaver"),//To do multiple assertion in multiple body methods works as HARD ASSERTION
+"support.text",equalTo("To keep ReqRes free, contributions towards server costs are appreciated!")
+
+
+
+
+     }
+}
